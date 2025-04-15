8cbf729d1a39fdca022b1258d90235e4128d456d
Your Real Name <belal@bentsgolf.com>
2025-04-16 01:34:05 +0700

    update

--- view/adminhtml/web/js/form/element/additional-select-source-mixin.js
+++ view/adminhtml/web/js/form/element/additional-select-source-mixin.js
@@ -10,17 +10,44 @@ define(
         var mixin = _.extend({
             defaults: {
                 useApi: 0,
+                canUseApi: false,
                 imports: {
                     toggleApi: '${$.ns}.${$.ns}.settings.use_api:value',
                 }
             },
             setOptionsAdv: function (value) {
-                // This function is now empty and gets controlled by toggleApi
+                var useApiSettings = reg.get('import_job_form.import_job_form.settings.use_api'),
+                    isApi = 0;
+
+                if (useApiSettings !== undefined) {
+                    this.canUseApi = true;
+                    isApi = useApiSettings.value();
+                }
+
+                if (this.sourceOptions == null) {
+                    this.sourceOptions = this.options();
+                }
+                var options = this.sourceOptions;
+                var prevValue = this.value();
+                var newOptions = [];
+                var self = this;
+                _.each(options, function (element, index) {
+                    if ((parseInt(isApi) !== parseInt(element.api) && self.canUseApi)) {
+                        return;
+                    }
+                    if (element.depends === "" || $.inArray(value, element.depends) !== -1) {
+                        newOptions.push(element);
+                    }
+                });
+                this.options(newOptions);
             },
 
             toggleApi: function (value) {
+                if (this.canUseApi) {
+                    return;
+                }
+
                 this.useApi = parseInt(value);
-                console.log('toggleApi', this.useApi);
 
                 // Ensure value exists
                 var typeFile = reg.get('import_job_form.import_job_form.source.type_file').value();
